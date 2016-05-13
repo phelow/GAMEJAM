75eba1c2305f008dc6e8fbd394be946c0189e1d1
phelow <wpheloung@gmail.com>
2016-05-13 17:32:06 -0400

    Firing projectiles

--- Game Jam/Assets/CharacterInput.cs
+++ Game Jam/Assets/CharacterInput.cs
@@ -5,10 +5,23 @@ public class CharacterInput : MonoBehaviour {
 	[SerializeField]Rigidbody2D _rb;
 
 	[SerializeField]private float _speed;
+	[SerializeField]private float _projectilePower = 10.0f;
+
+	[SerializeField]private GameObject _projectile;
 
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (PlayerInput());
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			GameObject projectile = GameObject.Instantiate (_projectile);
+			projectile.transform.position = transform.position;
+			Vector3 sp = Camera.main.WorldToScreenPoint (transform.position);
+			Vector3 dir = (Input.mousePosition - sp).normalized;
+			projectile.GetComponent<Rigidbody2D>().AddForce (dir * _projectilePower);
+		}
 	}
 
 	private IEnumerator PlayerInput(){
