0c77c78ec25339b87d9f524edfb843b2eed5ec8c
phelow <wpheloung@gmail.com>
2016-05-13 18:14:29 -0400

    Amulet is in

--- Game Jam/Assets/CharacterInput.cs
+++ Game Jam/Assets/CharacterInput.cs
@@ -11,9 +11,26 @@ public class CharacterInput : MonoBehaviour {
 	[SerializeField]private GameObject _projectile;
 	[SerializeField]private GameObject _melee;
 
+	[SerializeField]private float m_maxPower = 100.0f;
+	[SerializeField]private float m_curPower;
+	[SerializeField]private float m_drainRate = 10.0f;
+
+	[SerializeField]private float m_amuletRadius = 100.0f;
+	[SerializeField]private float m_amuletAngle = 90.0f;
+
+	[SerializeField]private float m_amuletPower = 10.0f;
+	[SerializeField]private float m_amuletMinStrength = 1.0f;
+	[SerializeField]private float m_amuletMaxStrength = 5.0f;
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (PlayerInput());
+		StartNight ();
+	}
+
+
+	void StartNight(){
+		m_curPower = m_maxPower;
 	}
 
 	void Update(){
@@ -32,22 +49,51 @@ public class CharacterInput : MonoBehaviour {
 			projectile.GetComponent<Rigidbody2D>().AddForce (dir * _meleePower);
 			
 		}
+		if (Input.GetKey (KeyCode.Space) && m_curPower > 0.0f) {
+			//Use the amulet
+			m_curPower -= Time.deltaTime * m_drainRate;
+
+			Collider2D[] hitColliders = Physics2D.OverlapCircleAll (transform.position, 100.0f);
+			Debug.Log (hitColliders.Length);
+			foreach (Collider2D col in hitColliders) {
+				//if the angle betweeen is less than amulet angle
+				Vector3 directionToTarget = transform.position - col.transform.position;
+
+				float angle = Vector3.Angle (transform.up, directionToTarget);
+
+				if (Mathf.Abs (angle) < 90.0f) {
+					Debug.Log ("Object is in front of the player");
+					//then push them away from the player.'
+					Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
+					if(rb){
+						rb.AddForce (-1 * transform.up * m_amuletPower * Mathf.Lerp(m_amuletMinStrength,m_amuletMaxStrength,m_curPower/m_maxPower));
+					}
+				}
+
+			}
+
+		}
 	}
 
 	private IEnumerator PlayerInput(){
 		while (true) {
 			if (Input.GetAxis ("Vertical") < -0.3f) {
-				_rb.AddRelativeForce (Vector2.down * _speed);
+				transform.up = Vector2.down;
+				_rb.AddRelativeForce (transform.up * _speed);
 			} else if (Input.GetAxis ("Vertical") > .3f) {
-				_rb.AddRelativeForce (Vector2.up * _speed);
+				transform.up = Vector2.up;
+				_rb.AddRelativeForce (transform.up * _speed);
 
 			}
 
 
 			if (Input.GetAxis ("Horizontal") < -0.3f) {
-				_rb.AddRelativeForce (Vector2.left * _speed);
+
+				transform.up = Vector2.left;
+				_rb.AddRelativeForce (transform.up * _speed);
 			} else if (Input.GetAxis ("Horizontal") > .3f) {
-				_rb.AddRelativeForce (Vector2.right * _speed);
+				transform.up = Vector2.right;
+				_rb.AddRelativeForce (transform.up * _speed);
 
 			}
 			yield return new WaitForEndOfFrame ();
---
+++ Game Jam/Assets/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	[SerializeField]GameObject _player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (_player.transform.position.x, _player.transform.position.y, _player.transform.position.z - 10.0f);
+	}
+}
