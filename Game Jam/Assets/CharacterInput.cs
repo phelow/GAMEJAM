6a67ce9d375079ce49f9856d7897ba95e5413304
phelow <wpheloung@gmail.com>
2016-05-13 15:39:04 -0400

    momentum based controller is live

---
+++ Game Jam/Assets/CharacterInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterInput : MonoBehaviour {
+	[SerializeField]Rigidbody2D _rb;
+
+	[SerializeField]private float _speed;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (PlayerInput());
+	}
+
+	private IEnumerator PlayerInput(){
+		while (true) {
+			if (Input.GetAxis ("Vertical") < -0.3f) {
+				_rb.AddRelativeForce (Vector2.down * _speed);
+			} else if (Input.GetAxis ("Vertical") > .3f) {
+				_rb.AddRelativeForce (Vector2.up * _speed);
+
+			}
+
+
+			if (Input.GetAxis ("Horizontal") < -0.3f) {
+				_rb.AddRelativeForce (Vector2.left * _speed);
+			} else if (Input.GetAxis ("Horizontal") > .3f) {
+				_rb.AddRelativeForce (Vector2.right * _speed);
+
+			}
+			yield return new WaitForEndOfFrame ();
+		}
+	}
+}
