5e6d3853a1fde9f105e2bce1e774df8fe208c95e
phelow <wpheloung@gmail.com>
2016-05-13 20:49:39 -0400

    Enemies will now die from projectiles

--- Game Jam/Assets/Enemy.cs
+++ Game Jam/Assets/Enemy.cs
@@ -7,14 +7,25 @@ public class Enemy : MonoBehaviour {
 	[SerializeField]protected Rigidbody2D m_rb;
 	[SerializeField]protected float m_speed;
 
+	[SerializeField]private float m_maxHealth = 100.0f;
+	[SerializeField]private float m_curHealth;
+
 
 	// Use this for initialization
 	void Start () {
 		s_player = GameObject.FindGameObjectWithTag ("Player");
+		m_curHealth = m_maxHealth;
 	}
 
 	protected void MoveTowardsPlayer(float dt){
 		m_rb.AddForce (m_speed * (s_player.transform.position - transform.position));
+	}
+
+	public void Damage(float dam){
+		m_curHealth -= dam;
+		if (m_curHealth < 0.0f) {
+			Destroy (this.gameObject);
+		}
 	}
 
 }
---
+++ Game Jam/Assets/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : Attack {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Enemy") {
+			coll.gameObject.GetComponent<Enemy> ().Damage (m_damage);
+		}
+		Destroy (this.gameObject);
+	}
+}
