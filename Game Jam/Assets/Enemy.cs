021d29158d54d8f54515b4550525edcfef407d6f
phelow <wpheloung@gmail.com>
2016-05-13 20:07:28 -0400

    explosive bob-omb style enemy added

---
+++ Game Jam/Assets/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	protected static GameObject s_player;
+
+	[SerializeField]protected Rigidbody2D m_rb;
+	[SerializeField]protected float m_speed;
+
+
+	// Use this for initialization
+	void Start () {
+		s_player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	protected void MoveTowardsPlayer(float dt){
+		m_rb.AddForce (m_speed * (s_player.transform.position - transform.position));
+	}
+
+}
---
+++ Game Jam/Assets/SuicideBomberEnemy.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuicideBomberEnemy : Enemy {
+
+	[SerializeField]private float m_explosionRange = 10.0f;
+	[SerializeField]private float m_explosionTrigger = 10.0f;
+	[SerializeField]private float m_explosionBuildupTime = 3.0f;
+
+	[SerializeField]private float m_minDamage = 1.0f;
+	[SerializeField]private float m_maxDamage = 10.0f;
+
+
+
+	private bool m_exploding = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (m_exploding == false) {
+			MoveTowardsPlayer (Time.deltaTime);
+			if (PlayerInExplodeRange ()) {
+				Explode ();
+			}
+		}
+	}
+
+
+	protected bool PlayerInExplodeRange(){
+		return (Vector3.Distance (s_player.gameObject.transform.position, transform.position) < m_explosionTrigger);
+	}
+
+	void Explode(){
+		m_rb.velocity = Vector2.zero;
+		m_exploding = true;
+		StartCoroutine (ExplodeRoutine ());
+	}
+
+	private IEnumerator ExplodeRoutine(){
+		yield return new WaitForSeconds (m_explosionBuildupTime);
+		float dist = Vector3.Distance (s_player.gameObject.transform.position, transform.position);
+		if (dist < m_explosionRange) {
+			Health.TakeDamage (Mathf.Lerp (m_minDamage, m_maxDamage, ((m_explosionRange - dist) / m_explosionRange)));
+		}
+		Destroy (this.gameObject);
+	}
+
+}
